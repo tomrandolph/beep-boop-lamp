bd13e05cdfefc2b89b09a051a32dcc7013cd931b
Tom Randolph <thomas.randolph26@gmail.com>
2025-12-14 00:51:38 -0800

    oops

--- bin/client.sh
+++ bin/client.sh
@@ -1,3 +1,3 @@
 source "$(dirname "$0")/../.env"
 
-npx mqtt pub -h $MQTT_HOST -i testing -u $MQTT_USERNAME -P $MQTT_PASSWORD -t esp001/state -C mqtts PULSE
+npx mqtt pub -h $MQTT_BROKER_HOST  -C $MQTT_BROKER_PROTOCOL -i testing -u $MQTT_USERNAME -P $MQTT_PASSWORD -t esp001/state  PULSE
